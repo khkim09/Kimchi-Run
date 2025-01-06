d6d8608e6d5e10c703599b8586f624c10525ab60
khkim09 <09ricky@naver.com>
2025-01-06 15:31:25 +0900

    add player double jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [Header("Settings")]
-    [SerializeField] public float jumpForce;
-    [SerializeField] public bool isJumping;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private int jumpCnt; // double jump 구현 위한 변수
 
     // 사전 작업
     /*
@@ -12,14 +12,23 @@ public class Player : MonoBehaviour
      * 'Player', 'Ground' - BoxCollider2D 생성 (collider 생성 - 경계면 생성, player 무한 낙하 방지)
      */
     [Header("References")]
-    [SerializeField] public Rigidbody2D playerRigidBody; // 'Player' 물리 법칙 적용
+    [SerializeField] private Rigidbody2D playerRigidBody; // 'Player' 물리 법칙 적용
 
     void Update()
     {
-        // "Jump" move
-        if (Input.GetKeyDown(KeyCode.Space)) // "space" key 입력 시
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCnt < 2) // double jump 구현
         {
-            playerRigidBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); // y축으로만 움직임 생성
+            playerRigidBody.linearVelocity = Vector2.zero; // 일정한 jump force를 위해 veloctiy 초기화 (내려오는 순간에서의 중력 무시를 위한 작업)
+            playerRigidBody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); // y축으로만 force 적용
+            jumpCnt++;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            jumpCnt = 0;
         }
     }
 }
